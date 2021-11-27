2b4e0f3ce7773b0ef9d3ae3158d0acdc62cfb012
Norihiro Kamae <norihiro@nagater.net>
2021-11-27 12:31:33 +0900

    Initial commit for Multi Source Effect

--- README.md
+++ README.md
@@ -1,61 +1,17 @@
-# OBS Plugin Template
+# OBS Multi Source Effect
 
 ## Introduction
 
-This plugin is meant to make it easy to quickstart development of new OBS plugins. It includes:
+This plugin blends two sources with a custom effect from a user-selectable file.
 
-- The CMake project file
-- Boilerplate plugin source code
-- A continuous-integration configuration for automated builds (a.k.a Build Bot)
+Some example effects are provided.
+- Add
+- Multiply
+- Screen
+- Color dodge
+- Linear burn
+- Color burn
+- Subtract
 
-## Configuring
-
-Open `CMakeLists.txt` and edit the following lines at the beginning:
-
-```cmake
-# Change `obs-plugintemplate` to your plugin's name in a machine-readable format
-# (e.g.: obs-myawesomeplugin) and set the value next to `VERSION` as your plugin's current version
-project(obs-plugintemplate VERSION 1.0.0)
-
-# Replace `Your Name Here` with the name (yours or your organization's) you want
-# to see as the author of the plugin (in the plugin's metadata itself and in the installers)
-set(PLUGIN_AUTHOR "Your Name Here")
-
-# Replace `com.example.obs-plugin-template` with a unique Bundle ID for macOS releases
-# (used both in the installer and when submitting the installer for notarization)
-set(MACOS_BUNDLEID "com.example.obs-plugintemplate")
-
-# Replace `me@contoso.com` with the maintainer email address you want to put in Linux packages
-set(LINUX_MAINTAINER_EMAIL "me@contoso.com")
-```
-
-## CI / Build Bot
-
-The CI scripts are made for Azure Pipelines. The sections below detail some of the common tasks possible with that CI configuration.
-
-### Retrieving build artifacts
-
-Each build produces installers and packages that you can use for testing and releases. These artifacts can be found a Build's page on Azure Pipelines.
-
-#### Building a Release
-
-Simply create and push a tag, and Azure Pipelines will run the pipeline in Release Mode. This mode uses the tag as its version number instead of the git ref in normal mode.
-
-### Signing and Notarizing on macOS
-
-On macOS, Release Mode builds will be signed and sent to Apple for notarization if `macosSignAndNotarize` is set to `True` at the top of the `azure-pipelines.yml` file. **You'll need a paid Apple Developer Account for this.**
-
-In addition to enabling `macosSignAndNotarize`, you'll need to setup a few more things for Signing and Notarizing to work:
-
-- On your Apple Developer dashboard, go to "Certificates, IDs & Profiles" and create two signing certificates:
-    - One of the "Developer ID Application" type. It will be used to sign the plugin's binaries
-    - One of the "Developer ID Installer" type. It will be used to sign the plugin's installer
-- Using the Keychain app on macOS, export these two certificates and keys into a .p12 file **protected with a strong password**
-- Add that `Certificates.P12` file as a [Secure File in Azure Pipelines](https://docs.microsoft.com/en-us/azure/devops/pipelines/library/secure-files?view=azure-devops) and make sure it is named `Certificates.p12`
-- Add the following secrets in your pipeline settings:
-    - `secrets.macOS.certificatesImportPassword`: Password of the .p12 file generated earlier
-    - `secrets.macOS.codeSigningIdentity`: Name of the "Developer ID Application" signing certificate generated earlier
-    - `secrets.macOS.installerSigningIdentity`: Name of "Developer ID Installer" signing certificate generated earlier
-    - `secrets.macOS.notarization.username`: Your Apple Developer Account's username
-    - `secrets.macOS.notarization.password`: Your Apple Developer Account's password
-    - `secrets.macOS.notarization.providerShortName`: Identifier (`Provider Short Name`, as Apple calls it) of the Developer Team to which the signing certificates belong. 
+## Acknowledgments
+- [Blend modes](https://github.com/Limeth/obs-shaderfilter-plus/discussions/29) - The plugin is inspired by this discussion.
---
+++ src/multisource-effect.c
@@ -0,0 +1,349 @@
+/*
+OBS Multi Source Effect
+Copyright (C) 2021 Norihiro Kamae <norihiro@nagater.net>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include <stdio.h>
+#include <obs-module.h>
+#include <util/dstr.h>
+
+#include "plugin-macros.generated.h"
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
+#define N_SRC 2
+
+struct msrc
+{
+	obs_source_t *self;
+
+	// properties and objects
+	char *effect_name;
+	gs_effect_t *effect;
+	char *src_name[N_SRC];
+	obs_weak_source_t *src_ref[N_SRC];
+
+	gs_texrender_t *texrender[N_SRC];
+	bool rendered[N_SRC];
+};
+
+static const char *msrc_get_name(void *unused)
+{
+	UNUSED_PARAMETER(unused);
+	return obs_module_text("Multi Source Effect");
+}
+
+static void msrc_update(void *, obs_data_t *);
+
+static void *msrc_create(obs_data_t *settings, obs_source_t *source)
+{
+	struct msrc *s = bzalloc(sizeof(struct msrc));
+	s->self = source;
+	msrc_update(s, settings);
+	return s;
+}
+
+static void msrc_destroy(void *data)
+{
+	struct msrc *s = data;
+
+	obs_enter_graphics();
+	gs_effect_destroy(s->effect);
+	for (int i = 0; i < N_SRC; i++)
+		gs_texrender_destroy(s->texrender[i]);
+	obs_leave_graphics();
+
+	for (int i = 0; i < N_SRC; i++)
+		obs_weak_source_release(s->src_ref[i]);
+
+	bfree(s->effect_name);
+	for (int i = 0; i < N_SRC; i++)
+		bfree(s->src_name[i]);
+
+	bfree(s);
+}
+
+void update_effect(struct msrc *s, const char *effect_name)
+{
+	if (!effect_name || !*effect_name)
+		return;
+
+	if (s->effect_name && strcmp(effect_name, s->effect_name) == 0)
+		return;
+
+	bfree(s->effect_name);
+	s->effect_name = bstrdup(effect_name);
+
+	obs_enter_graphics();
+	gs_effect_destroy(s->effect);
+	s->effect = gs_effect_create_from_file(effect_name, NULL);
+	if (!s->effect)
+		blog(LOG_ERROR, "Cannot load '%s'", effect_name);
+	else
+		blog(LOG_INFO, "Successfully loaded '%s'", effect_name);
+	obs_leave_graphics();
+}
+
+static void update_src(struct msrc *s, int ix, const char *src_name)
+{
+	if (!src_name || !*src_name)
+		return;
+
+	if (s->src_name[ix] && strcmp(src_name, s->src_name[ix]) == 0)
+		return;
+
+	bfree(s->src_name[ix]);
+	s->src_name[ix] = bstrdup(src_name);
+
+	obs_weak_source_release(s->src_ref[ix]);
+	s->src_ref[ix] = NULL;
+}
+
+static void msrc_update(void *data, obs_data_t *settings)
+{
+	struct msrc *s = data;
+
+	update_effect(s, obs_data_get_string(settings, "effect"));
+
+	for (int i = 0; i < N_SRC; i++) {
+		char name[16];
+		snprintf(name, sizeof(name), "src%d", i);
+		update_src(s, i, obs_data_get_string(settings, name));
+	}
+}
+
+static void msrc_get_defaults(obs_data_t *settings) {}
+
+#define FILE_FILTER "Effect Files (*.effect);;"
+
+static void properties_add_source(obs_properties_t *pp, const char *name, const char *desc)
+{
+	obs_properties_add_text(pp, name, desc, OBS_TEXT_DEFAULT);
+	// TODO: use obs_properties_add_list
+}
+
+static obs_properties_t *msrc_get_properties(void *data)
+{
+	struct msrc *s = data;
+
+	obs_properties_t *pp = obs_properties_create();
+	// obs_property_t *p;
+
+	struct dstr path = {0};
+	if (s && s->effect_name && *s->effect_name)
+		dstr_copy(&path, s->effect_name);
+	else {
+		dstr_init_move_array(&path, obs_module_file("examples"));
+		dstr_cat_ch(&path, '/');
+	}
+	obs_properties_add_path(pp, "effect", obs_module_text("Effect file"), OBS_PATH_FILE, FILE_FILTER, path.array);
+	dstr_free(&path);
+
+	for (int i = 0; i < N_SRC; i++) {
+		char name[16];
+		snprintf(name, sizeof(name), "src%d", i);
+		struct dstr desc = {0};
+		dstr_printf(&desc, obs_module_text("Source %d"), i);
+
+		properties_add_source(pp, name, desc.array);
+
+		dstr_free(&desc);
+	}
+
+	return pp;
+}
+
+static obs_source_t *msrc_get_source(struct msrc *s, int ix)
+{
+	if (s->src_ref[ix])
+		return obs_weak_source_get_source(s->src_ref[ix]);
+
+	if (!s->src_name[ix])
+		return NULL;
+
+	obs_source_t *src = obs_get_source_by_name(s->src_name[ix]);
+	s->src_ref[ix] = obs_source_get_weak_source(src);
+
+	return src;
+}
+
+static uint32_t msrc_get_width(void *data)
+{
+	struct msrc *s = data;
+
+	uint32_t ret = 0;
+	for (int i = 0; i < N_SRC; i++) {
+		obs_source_t *src = msrc_get_source(s, i);
+		if (!src)
+			continue;
+		uint32_t n = obs_source_get_width(src);
+		if (n > ret)
+			ret = n;
+		obs_source_release(src);
+	}
+	return ret;
+}
+
+static uint32_t msrc_get_height(void *data)
+{
+	struct msrc *s = data;
+
+	uint32_t ret = 0;
+	for (int i = 0; i < N_SRC; i++) {
+		obs_source_t *src = msrc_get_source(s, i);
+		if (!src)
+			continue;
+		uint32_t n = obs_source_get_height(src);
+		if (n > ret)
+			ret = n;
+		obs_source_release(src);
+	}
+	return ret;
+}
+
+static void msrc_enum_sources(void *data, obs_source_enum_proc_t enum_callback, void *param)
+{
+	struct msrc *s = data;
+	// TODO: implement me
+
+	for (int i = 0; i < N_SRC; i++) {
+		obs_source_t *src = msrc_get_source(s, i);
+		if (!src)
+			continue;
+		enum_callback(s->self, src, param);
+		obs_source_release(src);
+	}
+}
+
+static void msrc_render(void *data, gs_effect_t *effect)
+{
+	struct msrc *s = data;
+	UNUSED_PARAMETER(effect);
+
+	if (!s->effect)
+		return;
+
+	for (int i = 0; i < N_SRC; i++) {
+		if (s->rendered[i])
+			continue;
+
+		obs_source_t *src = msrc_get_source(s, i);
+		if (!src)
+			continue;
+
+		uint32_t w = obs_source_get_width(src);
+		uint32_t h = obs_source_get_height(src);
+
+		if (!s->texrender[i])
+			s->texrender[i] = gs_texrender_create(GS_BGRA, GS_ZS_NONE);
+		gs_texrender_reset(s->texrender[i]);
+		if (!gs_texrender_begin(s->texrender[i], w, h))
+			continue;
+
+		struct vec4 background;
+		vec4_zero(&background);
+
+		gs_clear(GS_CLEAR_COLOR, &background, 0.0f, 0);
+		gs_ortho(0.0f, (float)w, 0.0f, (float)h, -100.0f, 100.0f);
+
+		gs_blend_state_push();
+		gs_blend_function(GS_BLEND_ONE, GS_BLEND_ZERO);
+		obs_source_video_render(src);
+
+		gs_texrender_end(s->texrender[i]);
+
+		obs_source_release(src);
+
+		s->rendered[i] = true;
+	}
+
+	for (int i = 0; i < N_SRC; i++) {
+		if (!s->rendered[i])
+			continue;
+		gs_texture_t *tex = gs_texrender_get_texture(s->texrender[i]);
+		char name[16];
+		snprintf(name, sizeof(name), "src%d", i);
+		gs_effect_set_texture(gs_effect_get_param_by_name(s->effect, name), tex);
+	}
+
+	// FIXME: It is not efficient to loop all sources inside each function.
+	uint32_t ww = msrc_get_width(s);
+	uint32_t hh = msrc_get_height(s);
+
+	while (gs_effect_loop(s->effect, "Draw")) {
+		gs_draw_sprite(NULL, 0, ww, hh);
+	}
+}
+
+static void msrc_tick(void *data, float second)
+{
+	struct msrc *s = data;
+	UNUSED_PARAMETER(second);
+
+	for (int i = 0; i < N_SRC; i++)
+		s->rendered[i] = false;
+
+	for (int i = 0; i < N_SRC; i++) {
+		bool fail = false;
+		obs_source_t *src = msrc_get_source(s, i);
+
+		if (!src)
+			fail = true;
+
+		const char *name = obs_source_get_name(src);
+		if (!name || s->src_name[i])
+			fail = true;
+		else if (strcmp(name, s->src_name[i]) != 0)
+			fail = true;
+
+		obs_source_release(src);
+
+		if (fail) {
+			obs_weak_source_release(s->src_ref[i]);
+			s->src_ref[i] = NULL;
+		}
+	}
+}
+
+static struct obs_source_info msrc_info = {
+	.id = "net.nagater.obs-multisource-effect",
+	.type = OBS_SOURCE_TYPE_INPUT,
+	.output_flags = OBS_SOURCE_VIDEO | OBS_SOURCE_CUSTOM_DRAW,
+	.get_name = msrc_get_name,
+	.create = msrc_create,
+	.destroy = msrc_destroy,
+	.update = msrc_update,
+	.get_defaults = msrc_get_defaults,
+	.get_properties = msrc_get_properties,
+	.get_width = msrc_get_width,
+	.get_height = msrc_get_height,
+	.enum_active_sources = msrc_enum_sources,
+	.video_render = msrc_render,
+	.video_tick = msrc_tick,
+};
+
+bool obs_module_load(void)
+{
+	obs_register_source(&msrc_info);
+	blog(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+	return true;
+}
+
+void obs_module_unload()
+{
+	blog(LOG_INFO, "plugin unloaded");
+}
